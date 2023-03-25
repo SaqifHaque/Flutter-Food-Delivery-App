77e3fbd0488640958287e2793cce34717306ec89
Saqif <saqifhaque@gmail.com>
2023-03-25 17:50:50 +0600

    working on App Bar

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:food_delivery_app/constants/colors.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var selected = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Text('Home Page'),
+      backgroundColor: kBackground,
+      body: Column (
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          CustomAppBar()
+        ]
+
       )
-    );
+    )
   }
 }
