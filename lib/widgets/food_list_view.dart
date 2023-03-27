0a07cef24e6123e671f33791e9ce21c9f00868b6
Saqif <saqifhaque@gmail.com>
2023-03-27 22:49:13 +0600

    working on mapping food item widget

--- lib/widgets/food_list_view.dart
+++ lib/widgets/food_list_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery_app/models/restaurant.dart';
+import 'package:food_delivery_app/widgets/food_item.dart';
 
 class FoodListView extends StatelessWidget {
   final int selected;
@@ -11,8 +12,21 @@ class FoodListView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final category = restaurant.menu.keys.toList();
     return Container(
-      child: Text("ListView")
+      padding: EdgeInsets.symmetric(horizontal: 25),
+      child: PageView(
+        controller: pageController,
+        onPageChanged: (index) => callback(index),
+        children: category.map((e) =>
+            ListView.separated(
+              itemBuilder: (context, index) => FoodItem(
+                restaurant.menu[category[selected]]![index]
+              ),
+              separatorBuilder: (_, index) => SizedBox(height: 15),
+              itemCount: restaurant.menu[category[selected]]!.length,
+            )).toList(),
+      )
     );
   }
 }
